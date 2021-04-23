f354c049e98fe3c2a58d22dd934db96e5353a833
saadmanfarhad <saadmanheavy@gmail.com>
2021-04-23 15:44:50 +0600

    Made password optional for handling social login

--- api/models.py
+++ api/models.py
@@ -8,10 +8,10 @@ from django.utils import timezone
 class CustomUser(AbstractBaseUser):
     email = models.EmailField(verbose_name='email', max_length=60, unique=True)
     username = models.CharField(max_length=30, blank=True, null=True)
-    password = models.CharField(max_length=255)
+    password = models.CharField(max_length=255, blank=True, null=True)
     first_name = models.CharField(_('first name'), max_length=30, blank=True, null=True)
     last_name = models.CharField(_('last name'), max_length=30, blank=True, null=True)
-    avatar = models.CharField(_('avatar'), max_length=30, blank=True, null=True)
+    avatar = models.CharField(_('avatar'), max_length=255, blank=True, null=True)
     date_of_birth = models.DateField(_('date of birth'), blank=True, null=True)
     about = models.TextField(_('about'), blank=True, null=True)
     is_staff = models.BooleanField(default=False)
